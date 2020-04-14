bdd5466e7cbd1f4b7d5b9ced098c5cfde7b64ed1
shirleywu915 <57686887+shirleywu915@users.noreply.github.com>
2020-04-14 14:08:27 -0400

    modified Bank to function better

--- Games/Bank.py
+++ Games/Bank.py
@@ -9,6 +9,8 @@ Practical test
 print("WELCOME to ICS Bank!")
 
 pin = "190307"
+Checking = 253
+Saving = 6250
 
 user = input("Please enter your PIN: ") #Ask for pin
 attemps = 2
@@ -17,110 +19,104 @@ while user != pin and attemps > 0:
     print("Wrong PIN. %d attemps remaining." %attemps) #Ask for pin again
     attemps -= 1
     user = input("Please enter your PIN again: ")
-if attemps == 0:
+if attemps < 0:
     print("Tries exceeded. Account is now locked.")
 
 #allow access to account
 if user == pin:
-    print("Now you have access to your account.")
+    print("Now you have access to your account.\n")
+    
+def start_banking(c,s):
     selections = ["Make a selection:", "1. View Balance","2. Make a Withdrawal","3. Make a Deposite","4. Transfer Money", "0 to Exit."]
     for i in selections:
         print(i)
     selection = int(input(">>> "))
 
 #define each actions
-    a = 253
-    b = 6250
 
-    def end (x,y):
-        the_end = "\nThanks for banking!\nReceipt:\n-------------------\nChecking Account: $" + str(x) + "\nSaving Account : $" + str(y)
-        return the_end
+    def end (c,s):
+        the_end = ("\nThanks for banking!\n\n-------- Receipt --------\nChecking Account: $ %.2f \nSaving Account : $ %.2f" %(c, s))
+        print(the_end)
+        return ("")
     
-    def view_balance ():
-        account = input("From Account: \n1. Checking\n2. Savings\n>>> ")
+    def view_balance (c,s):
+        account = input("\nFrom Account: \n1. Checking\n2. Savings\n>>> ")
         if account == '1':
-            a =("Checking Account : $253")
-            return a
+            a =("Checking Account : $ %.2f" %c)
+            print(a)
+            return (continue_banking(c,s))
         elif account == '2':
-            b =("Saving Account: $6250")
-            return b
-        return end
+            b =("Saving Account: $ %.2f" %s)
+            print(b)
+            return (continue_banking(c,s))
     
-    def withdrawal ():
+    def withdrawal (c,s):
         account = input("From Account: \n1. Checking\n2. Savings\n>>> ")
         if account == '1':
             amount = float(input("Enter an amount to withdraw: "))
-            c_money = a - amount
-            s_money = b
-            return("Checking account balance: $%.2f"%c_money)
+            c -= amount
+            print("Checking account balance: $%.2f"%c)
+            return (continue_banking(c,s))
         elif account == '2':
             amount = float(input("Enter an amount to withdraw: "))
-            s_money = b - amount
-            c_money = a
             ask = input("$4 fee for withdrawing from Savings. Continue? Y/N >>> ")
             if ask  == "Y":
-                return("Saving account balance: $%.2f"%s_money)
+                s -= (amount+4)
+                print("Saving account balance: $%.2f"%s)
+                return (continue_banking(c,s))
+            else:
+                return (continue_banking(c,s))
             
-        continue_banking = input("Continue Banking? Y/N >>>")
-        if continue_banking == 'Y':
-            for i in selections:
-                print(i)
-            selection = int(input(">>> "))
+    def continue_banking (c,s):
+        continue_b = input("\nContinue Banking? Y/N >>>")
+        if continue_b == 'Y':
+            return (start_banking(c,s))
         else:
-            return(end(c_money,s_money))
-            
+            return (end(c,s))
 
-    def deposit():
+    def deposit(c,s):
         account = input("From Account: \n1. Checking\n2. Savings\n>>> ")
         if account == '1':
             amount = float(input("Enter an amount to deposite: "))
-            c_money = a + amount
-            s_money = b
-            return("Checking account balance: $%.2f"%c_money)
+            c += amount
+            print("Checking account balance: $%.2f"%c)
+            return (continue_banking(c,s))
         elif account == '2':
             amount = float(input("Enter an amount to deposite: "))
-            s_money = b + amount
-            c_money = a
-            return("Saving account balance: $%.2f"%s_money)
-        continue_banking = input("Continue Banking? Y/N >>>")
-        if continue_banking == 'Y':
-            for i in selections:
-                print(i)
-            selection = int(input(">>> "))
-        else:
-            return(end(c_money,s_money))
-            
+            s +=amount
+            print("Saving account balance: $%.2f"%s)
+            return (continue_banking(c,s))
 
-    def transfer():
+    def transfer(c,s):
         account = input("From Account: \n1. Checking\n2. Savings\n>>> ")
         if account == '1':
             amount = float(input("Enter an amount to transfer: "))
-            c_money = a - amount
-            s_money = a + amount
-            return("Checking account balance: $%.2f"%c_money)
-            return("Saving account balance: $%.2f"%s_money)
+            c -= amount
+            s += amount
+            print("Checking account balance: $%.2f \nSaving account balance: $%.2f" %(c,s))
+            return (continue_banking(c,s))
         elif account == '2':
             amount = float(input("Enter an amount to transfer: "))
-            c_money = a + amount
-            s_money = a - amount
-            return("Checking account balance: $%.2f"%c_money)
-            return("Saving account balance: $%.2f"%s_money)
-        continue_banking = input("Continue Banking? Y/N >>>")
-        if continue_banking == 'Y':
-            for i in selections:
-                print(i)
-            selection = int(input(">>> "))
-        else:
-            return(end(c_money,s_money))
+            c += amount
+            s -= amount
+            print("Checking account balance: $%.2f \nSaving account balance: $%.2f" %(c,s))
+            return (continue_banking(c,s))
             
 
 #return results
     if selection == 1:
-        print(view_balance())
+        print(view_balance(c,s))
     elif selection == 2:
-        print(withdrawal())
+        print(withdrawal(c,s))
     elif selection == 3:
-        print(deposit())
+        print(deposit(c,s))
     elif selection == 4:
-        print(transfer())
-    
+        print(transfer(c,s))
+    elif selection == 0:
+        print(end(c,s))
+    else:
+        print("INVALID INPUT")
+    return ("")
+
+#begin program
+print(start_banking(Checking, Saving))
